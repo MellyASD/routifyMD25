920b531b86e3a71f08f153ca157ffecfb0cf5263
MellyASD <melissaramirezortegon@hotmail.com>
2025-11-13 20:36:59 -0500

    [FIX] Simulator provider was added to transport module

--- src/modules/transport/transport.module.ts
+++ src/modules/transport/transport.module.ts
@@ -3,11 +3,12 @@ import { TransportService } from './transport.service';
 import { TransportController } from './transport.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transport } from 'src/entities/transport.entity';
+import { TransportSimulatorService } from '../services/transport-simulator.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Transport])],
   controllers: [TransportController],
-  providers: [TransportService],
+  providers: [TransportService, TransportSimulatorService],
   exports: [TransportService],
 })
 export class TransportModule {}
