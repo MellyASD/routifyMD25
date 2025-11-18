14eb68a6f5ad946bc587fd8ab6be74e3b7d5d8a2
Alejandra Gutierrez <98alejag@gmail.com>
2025-11-18 18:26:22 -0500

    [FEAT] JWTGuard

--- src/modules/transport/transport.controller.ts
+++ src/modules/transport/transport.controller.ts
@@ -1,19 +1,20 @@
-import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards, ParseIntPipe } from '@nestjs/common';
 import { TransportService } from './transport.service';
-import { CreateTransportDTO } from 'src/dto/create-transport.dto';
+import { CreateRouteDTO } from 'src/dto/create-route.dto';
 import { UserRole } from 'src/entities/user.entity';
 import { Roles } from '../auth/roles.decorator';
 import { RolesGuard } from '../auth/roles.guard';
 import { CompareTransportDTO } from 'src/dto/compare-transport.dto';
+import { JwtAuthGuard } from '../auth/jwt.guard';
 @Controller('transport')
 export class TransportController {
   constructor(private readonly transportService: TransportService) {}
 
-  @Post()
-  @UseGuards(RolesGuard)
+  @Post('route')
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.ADMIN, UserRole.TRUCKER)
-  create(@Body() createTransportDto: CreateTransportDTO) {
-    return this.transportService.create(createTransportDto);
+  create(@Body() createRouteDto: CreateRouteDTO) {
+    return this.transportService.create(createRouteDto);
   }
 
   @Get()
@@ -22,7 +23,7 @@ export class TransportController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: number) {
     return this.transportService.findOne(id);
   }
 
