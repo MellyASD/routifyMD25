67e2c153c43dfd7849fb05812b634e6d50ec11a2
Lauramarin-220 <lauramarin220x@gmail.com>
2025-11-18 19:53:54 -0500

    [UPDATE]  Create- Comparison Validation and Swagger

--- src/dto/create-comparison.dto.ts
+++ src/dto/create-comparison.dto.ts
@@ -1,12 +1,31 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
+import { IsAddressOrCity } from "src/common/validators/is-address-or-city.validator";
 
 export class CreateComparisonDTO {
+  @ApiProperty({
+      description: 'Origin address or city',
+      example: 'Bogota DC',
+    })
 
   @IsString()
   @IsNotEmpty()
+  @IsAddressOrCity({
+      message:
+        'Origin must be a valid address or city Example "Bogota DC o Cra 24 #45"',
+    })
   origin: string;
 
+  @ApiProperty({
+    description: 'Destination address or city',
+    example: 'Calle 50 #30',
+  })
+
   @IsString()
   @IsNotEmpty()
+  @IsAddressOrCity({
+    message:
+      'Destination must be a valid address or city, Example "Bogota DC o Cra 24 #45"',
+  })
   destination: string;
 }
